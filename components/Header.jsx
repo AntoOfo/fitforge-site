3ec6f8f9502547f1d1c1513235d9d899ff882cf0
AntoOfo <148264049+AntoOfo@users.noreply.github.com>
2025-09-20 19:56:34 +0100

    header html done, working on styles

--- components/Header.jsx
+++ components/Header.jsx
@@ -1,7 +1,16 @@
+import Logo from "../images/sitelogo.png"
+
 export default function Header() {
     return (
-        <header>
-            
+        <header className="flex items-center justify-between px-40 h-24 w-full bg-white">
+            <img src={Logo} className="w-24"/> 
+            <nav className="flex ml-[483px] gap-9">
+                <a>HOME</a>
+                <a>ABOUT</a>
+                <a>SERVICES</a>
+                <a>PRICING</a>
+                <a>CONTACT</a>
+            </nav>
         </header>
     )
 }
