aa009dbbcc0a46e9e0358ee3f658f533e1611f21
AntoOfo <148264049+AntoOfo@users.noreply.github.com>
2025-09-20 21:23:29 +0100

    new upscaled hero img added, hero file turned to hero section so both header and hero take up full viewport.working on text

--- App.jsx
+++ App.jsx
@@ -1,11 +1,10 @@
 import Header from "./components/Header"
-import Hero from "./components/Hero"
+import HeroSection from "./components/Hero"
 
 export default function App() {
     return (
         <>
-        <Header />
-        <Hero />
+        <HeroSection />
         </>
     )
 }
--- components/Hero.jsx
+++ components/Hero.jsx
@@ -1,5 +1,17 @@
-export default function Hero() {
+import Header from "./Header"
+import HeroImg from "../images/hero_img.jpg"
+
+export default function HeroSection() {
     return (
-        <h1>im the hero lol</h1>
-    )
+        <div className="w-full h-screen relative">
+            <Header />
+
+            <div 
+            className="w-full h-[calc(100vh-96px)] bg-cover bg-center bg-[rgba(100,0,0,0.48)] bg-blend-overlay"
+            style={{ backgroundImage: `url(${HeroImg})` }}>
+                <h1>Transform Your Body, Transform Your Life</h1>
+            </div>
+            
+        </div>
+    );
 }
