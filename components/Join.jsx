08ddda89a1528a2a2f41d4df20686c2416825482
AntoOfo <148264049+AntoOfo@users.noreply.github.com>
2025-09-21 18:07:54 +0100

    data folder created to avoid duplication. join us data populated. will map in jsx

--- components/Join.jsx
+++ components/Join.jsx
@@ -1,5 +1,17 @@
+
 export default function Join() {
     return (
-        <h1>im the join</h1>
+        <section className="bg-blue-700">
+            <div>
+                <h2>Why Join Us?</h2>
+                <span>
+                    <div>
+                        <img src={} />
+                        <h3>Flexible Plans</h3>
+                    </div>
+
+                </span>
+            </div>
+        </section>
     )
 }
---
+++ data/joinData.js
@@ -0,0 +1,27 @@
+import Page from "../images/planning.png"
+import Door from "../images/house-door.png"
+import Trainer from "../images/trainer.png"
+import Dumbbell from "../images/dumbbell.png"
+
+export const joinData = [
+    {
+        image: Page,
+        text: "Flexible Plans"
+    },
+
+    {
+        image: Door,
+        text: "24/7 Access"
+    },
+
+    {
+        image: Trainer,
+        text: "Expert Trainers"
+    },
+
+    {
+        image: Dumbbell,
+        text: "Elite Gear"
+    }
+    
+]
