e6073318f8dc92cc301a95511258792d06a9866e
AntoOfo <148264049+AntoOfo@users.noreply.github.com>
2025-09-21 23:29:30 +0100

    card and carousel imported and implemented into mission. path imported in vite config

--- components/Mission.jsx
+++ components/Mission.jsx
@@ -3,6 +3,9 @@ import ImageTwo from "../images/mission_2.jpg"
 import ImageThree from "../images/mission_3.jpg"
 import ImageFour from "../images/mission_4.jpg"
 
+import { Card, CardContent } from "@/components/ui/card"
+import { Carousel, CarouselContent, CarouselItem, CarouselNext, CarouselPrevious } from "@/components/ui/carousel"
+
 export default function Mission() {
     const images = [ImageOne, ImageTwo, ImageThree, ImageFour]
     return (
@@ -12,7 +15,24 @@ export default function Mission() {
                     <h2 className="font-bold text-[40px] text-[rgba(192,57,43,1)]">Our Mission</h2>
                     <p className="mt-9 font-normal text-2xl text-[rgba(0,0,0,0.7)]">We are the catalyst for your best life, not just a facility. We believe true fitness is built on confidence, consistency, and community. We commit to providing expert guidance, a judgment-free environment, and the tools you need to not just meet your goals, but to redefine your potential and sustain lifelong well-being.</p>
                 </div>
-                <img src={ImageOne} className="w-80 h-80 object-cover rounded-4xl" />
+
+                <div>
+                    <Carousel className="w-full">
+                        <CarouselContent>
+                            {images.map((imgSrc, index) => (
+                                <CarouselItem key={index}>
+                                    <Card>
+                                        <CardContent className="p-0">
+                                            <img src={imgSrc} className="w-80 h-80 object-cover rounded-4xl" />
+                                        </CardContent>
+                                    </Card>
+                                </CarouselItem>
+                            ))}
+                        </CarouselContent>
+                        <CarouselPrevious />
+                        <CarouselNext />
+                    </Carousel>
+                </div>
             </div>
         </section>
     )
