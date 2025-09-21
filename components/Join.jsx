0df3681c0ef0af132b78e8f205962fd743cee513
AntoOfo <148264049+AntoOfo@users.noreply.github.com>
2025-09-21 18:28:20 +0100

    font sizing, weight, colour and img size set

--- components/Join.jsx
+++ components/Join.jsx
@@ -2,14 +2,18 @@ import { joinData } from "../data/joinData"
 
 export default function Join() {
     return (
-        <section className="bg-blue-700">
+        <section className="flex flex-col font-[Poppins]">
             <div>
-                <h2>Why Join Us?</h2>
+                <h2 className="font-bold text-[40px] text-[rgba(192,57,43,1)]">
+                    Why Join Us?
+                </h2>
                 <span>
                     {joinData.map(join => (
                         <div>
-                            <img src={join.image} />
-                            <h3>{join.text}</h3>
+                            <img src={join.image} className="w-[75px]" />
+                            <h3 className="font-medium text-2xl text-[rgba(192,57,43,1)]">
+                                {join.text}
+                            </h3>
                         </div>
                     ))}
                 </span>
