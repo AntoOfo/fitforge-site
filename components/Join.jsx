f1570ce74e57b4fd2b680074a438522102aecb80
AntoOfo <148264049+AntoOfo@users.noreply.github.com>
2025-09-21 19:09:57 +0100

    join us section sorted for now, will worry about responsiveness when all sections are good

--- components/Join.jsx
+++ components/Join.jsx
@@ -2,21 +2,21 @@ import { joinData } from "../data/joinData"
 
 export default function Join() {
     return (
-        <section className="flex flex-col font-[Poppins]">
+        <section className="mt-16 w-full px-8 flex flex-col items-center font-[Poppins]">
             <div>
-                <h2 className="font-bold text-[40px] text-[rgba(192,57,43,1)]">
+                <h2 className="text-center font-bold text-[40px] text-[rgba(192,57,43,1)]">
                     Why Join Us?
                 </h2>
-                <span>
+                <div className="mt-12 mb-16 flex justify-between gap-48 w-full">
                     {joinData.map(join => (
-                        <div>
+                        <div className="flex flex-col items-center">
                             <img src={join.image} className="w-[75px]" />
                             <h3 className="font-medium text-2xl text-[rgba(192,57,43,1)]">
                                 {join.text}
                             </h3>
                         </div>
                     ))}
-                </span>
+                </div>
             </div>
         </section>
     )
