d197be0eb868ce64a68887b0059dd8c4a530d459
AntoOfo <148264049+AntoOfo@users.noreply.github.com>
2025-09-22 00:22:50 +0100

    wrapper div to fix clipped edge

--- components/Mission.jsx
+++ components/Mission.jsx
@@ -15,22 +15,22 @@ export default function Mission() {
                     <h2 className="font-bold text-[40px] text-[rgba(192,57,43,1)]">Our Mission</h2>
                     <p className="mt-9 font-normal text-2xl text-[rgba(0,0,0,0.7)]">We are the catalyst for your best life, not just a facility. We believe true fitness is built on confidence, consistency, and community. We commit to providing expert guidance, a judgment-free environment, and the tools you need to not just meet your goals, but to redefine your potential and sustain lifelong well-being.</p>
                 </div>
-
-                <div>
-                    <Carousel className="w-full">
-                        <CarouselContent>
+                
+                <div className="w-full h-80 rounded-4xl overflow-hidden">
+                    <Carousel className="w-full h-full">
+                        <CarouselContent className="h-full">
                             {images.map((imgSrc, index) => (
-                                <CarouselItem key={index}>
-                                    <Card>
-                                        <CardContent className="p-0">
-                                            <img src={imgSrc} className="w-80 h-80 object-cover rounded-4xl" />
+                                <CarouselItem key={index} className="basis-full h-full">
+                                    <Card className="p-0 w-full h-full">
+                                        <CardContent className="p-0 w-full h-full">
+                                            <img src={imgSrc} className="w-80 h-80 object-cover" />
                                         </CardContent>
                                     </Card>
                                 </CarouselItem>
                             ))}
                         </CarouselContent>
-                        <CarouselPrevious />
-                        <CarouselNext />
+                        <CarouselPrevious  className="absolute left-3 top-1/2 -translate-y-1/2 z-20"/>
+                        <CarouselNext className="absolute right-3 top-1/2 -translate-y-1/2 z-20" />
                     </Carousel>
                 </div>
             </div>
