483d859d558d9e7a0ef19fe17d1f1e03bf9aa8fd
AntoOfo <148264049+AntoOfo@users.noreply.github.com>
2025-09-21 19:41:11 +0100

    file created for mission section and imported into app

--- App.jsx
+++ App.jsx
@@ -1,12 +1,14 @@
 import Header from "./components/Header"
 import HeroSection from "./components/Hero"
 import Join from "./components/Join"
+import Mission from "./components/Mission"
 
 export default function App() {
     return (
         <>
         <HeroSection />
         <Join />
+        <Mission />
         </>
     )
 }
---
+++ components/Mission.jsx
@@ -0,0 +1,5 @@
+export default function Mission() {
+    return (
+        <h1>Sup, im the mission section</h1>
+    )
+}
