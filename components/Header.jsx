a6fd92c2efe3a95b03a66aebe5bc29f17e67ba99
AntoOfo <148264049+AntoOfo@users.noreply.github.com>
2025-09-21 19:18:34 +0100

    header position set as sticky

--- components/Header.jsx
+++ components/Header.jsx
@@ -2,9 +2,9 @@ import Logo from "../images/sitelogo.png"
 
 export default function Header() {
     const navLinks = ["HOME", "ABOUT", "SERVICES", "PRICING", "CONTACT"];
-    
+
     return (
-        <header className="flex items-center justify-between px-40 h-20 w-full bg-white">
+        <header className="sticky top-0 flex items-center justify-between px-40 h-20 w-full bg-white">
             <img src={Logo} className="w-20"/> 
             <nav className="flex ml-[483px] gap-9 font-[Poppins] font-normal text-lg">
                 {navLinks.map(link => ( 
