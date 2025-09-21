abef71c25548b9c57fd7b9a3c1a3a4059f065149
AntoOfo <148264049+AntoOfo@users.noreply.github.com>
2025-09-21 17:45:43 +0100

    file created for join us section

--- App.jsx
+++ App.jsx
@@ -1,10 +1,12 @@
 import Header from "./components/Header"
 import HeroSection from "./components/Hero"
+import Join from "./components/Join"
 
 export default function App() {
     return (
         <>
         <HeroSection />
+        <Join />
         </>
     )
 }
---
+++ components/Join.jsx
@@ -0,0 +1,5 @@
+export default function Join() {
+    return (
+        <h1>im the join</h1>
+    )
+}
