c1af23bc858d63a1d2958c25b43e8b05c7632bcc
AntoOfo <148264049+AntoOfo@users.noreply.github.com>
2025-09-22 17:38:39 +0100

    constructing rough explore section, gonna bring in another bigger carousel

--- components/Explore.jsx
+++ components/Explore.jsx
@@ -1,5 +1,18 @@
+import Placeholder from "../images/mission_img.jpg"
+
 export default function Explore() {
     return (
-        <h1>explore section !!!</h1>
+        <section className="w-full font-[Poppins] bg-amber-300">
+            <div className="flex flex-col items-center">
+                <h2 className="font-bold text-[40px] text-[rgba(192,57,43,1)]">Explore The Gym</h2>
+                <div>
+                    <div>
+                        <img src={Placeholder} className="w-[572px] h-[348px]"/>
+                        <h3 className="font-bold text-[32px] text-[rgba(192,57,43,0.8)]">Cardio Equipment</h3>
+                        <p className="w-[520px] font-normal text-[20px] text-[rgba(0,0,0,0.8)]">Get your heart pumping with our top-of-the-line treadmills, bikes, cross trainers, and rowers ideal for endurance, fat burn, and overall fitness.</p>
+                    </div>
+                </div>
+            </div>
+        </section>
     )
 }
