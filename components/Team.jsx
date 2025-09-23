382b14c363795a1302fa94ecb65a9b31a7bb4e20
AntoOfo <148264049+AntoOfo@users.noreply.github.com>
2025-09-23 21:59:30 +0100

    shadow and padding changes to team component and shadcn separator imported

--- components/Team.jsx
+++ components/Team.jsx
@@ -7,7 +7,7 @@ export default function Team() {
                 <h1 className="font-bold text-[40px] text-[rgba(192,57,43,1)]">Meet The Team</h1>
                 <div className="flex justify-between mt-[72px]">
                     {teamData.map(team =>
-                        <div className="justify-items-center py-9 px-4 rounded-[134px] bg-[rgba(239,239,239,1)] shadow-[0_4px_15.1px_1px_rgba(0,0,0,0.15)]">
+                        <div className="justify-items-center py-12 px-4 rounded-[134px] bg-[rgba(239,239,239,1)] shadow-[0_2px_10px_1px_rgba(0,0,0,0.15)]">
                             <img src={team.image} className="w-[149px] h-[149px] rounded-full object-cover shadow-[0_4px_15.1px_1px_rgba(0,0,0,0.15)]" />
                             <h3 className="mt-[18px] font-semibold text-[20px] text-[rgba(192,57,43,1)]">{team.name}</h3>
                             <p className="font-medium italic text-[16px] text-[rgba(0,0,0,0.6)]">{team.role}</p>
