ef0be38047268ea2c64e640f434fd5bbd2805956
AntoOfo <148264049+AntoOfo@users.noreply.github.com>
2025-09-23 17:26:44 +0100

    new font url added to include more font weights and building membership section

--- components/Membership.jsx
+++ components/Membership.jsx
@@ -1,5 +1,15 @@
 export default function Membership() {
     return (
-        <h1>membership section</h1>
+        <section className="w-full bg-[rgba(192,57,43,1)] font-[Poppins]">
+            <div className="ml-32 mr-32">
+                <h1 className="font-bold text-[40px] text-white">Membership Plans</h1>
+                <p className="font-light text-[20px] text-[rgba(255,255,255,0.85)]">Find the commitment level that fits your goals and budget.</p>
+                <div className="flex">
+                    <div className="w-[354px] h-[316px] bg-white">
+                        
+                    </div>
+                </div>
+            </div>
+        </section>
     )
 }
