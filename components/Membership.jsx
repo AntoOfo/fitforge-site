b94d0ddaf7b6b4faf5931583d29c479dd0ce4e0f
AntoOfo <148264049+AntoOfo@users.noreply.github.com>
2025-09-23 20:10:49 +0100

    membership data mapped to the jsx completing the section for now

--- components/Membership.jsx
+++ components/Membership.jsx
@@ -1,3 +1,5 @@
+import { membershipData } from "../data/membershipData"
+
 export default function Membership() {
     return (
         <section className="w-full py-20 bg-[rgba(192,57,43,1)] font-[Poppins]">
@@ -5,21 +7,22 @@ export default function Membership() {
                 <h1 className="font-bold text-[40px] text-white">Membership Plans</h1>
                 <p className="font-light text-[20px] text-[rgba(255,255,255,0.85)]">Find the commitment level that fits your goals and budget.</p>
                 <div className="flex justify-between">
-
-                    <div className="mt-12 px-5 py-6 rounded-2xl bg-white">
-                        <p className="font-bold text-[20px]">Month-to-Month</p>
-                        <span className="mt-[3px] mb-0.5 flex items-baseline">
-                            <h1 className="font-bold text-[64px] text-[rgba(192,57,43,1)]">$89</h1>
-                            <p className="font-medium text-[20px] text-[rgba(0,0,0,0.7)]">/month</p>
-                        </span>
-                        <p className="font-medium text-[20px] w-[355px] text-[rgba(0,0,0,0.7)]">Full facility access, unlimited classes. Cancel anytime.</p>
-                        <div className="mt-4 flex justify-end">
-                            <button className="bg-white border-2 border-[rgba(192,57,43,1)] rounded-[15px] px-7 py-1.5  font-bold text-[20px] text-[rgba(192,57,43,1)] hover:cursor-pointer">
-                            Select
-                            </button>
+                    {membershipData.map(membership => (
+                        <div className="mt-12 px-5 py-6 rounded-2xl bg-white">
+                            <p className="font-bold text-[20px]">{membership.type}</p>
+                            <span className="mt-[3px] mb-0.5 flex items-baseline">
+                                <h1 className="font-bold text-[64px] text-[rgba(192,57,43,1)]">£{membership.price}</h1>
+                                <p className="font-medium text-[20px] text-[rgba(0,0,0,0.7)]">/month</p>
+                            </span>
+                            <p className="font-medium text-[20px] w-[355px] text-[rgba(0,0,0,0.7)]">{membership.desc}</p>
+                            <div className="mt-4 flex justify-end">
+                                <button className="bg-white border-2 border-[rgba(192,57,43,1)] rounded-[15px] px-7 py-1.5  font-bold text-[20px] text-[rgba(192,57,43,1)] hover:cursor-pointer">
+                                Select
+                                </button>
+                            </div>
                         </div>
-                    </div>
-                    
+                    ))}
+
                 </div>
             </div>
         </section>
