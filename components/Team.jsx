cc546f774be784ad71b75b70bf5493ae412eda4c
AntoOfo <148264049+AntoOfo@users.noreply.github.com>
2025-09-23 20:56:53 +0100

    building single team member section

--- components/Team.jsx
+++ components/Team.jsx
@@ -1,5 +1,19 @@
+import Placeholder from "../images/team_1.jpg"
+
 export default function Team() {
     return (
-        <h1>im the team section lol</h1>
+        <section className="w-full py-20 font-[Poppins]">
+            <div className="ml-32 mr-32">
+                <h1 className="font-bold text-[40px] text-[rgba(192,57,43,1)]">Meet The Team</h1>
+                <div className="flex">
+                    <div className="w-[223px] h-[455px] rounded-[134px] bg-[rgba(239,239,239,1)]">
+                        <img src={Placeholder} className="w-[149px] h-[149px] rounded-full object-cover" />
+                        <h3 className="font-semibold text-[20px] text-[rgba(192,57,43,1)]">Marcus Vance</h3>
+                        <p className="font-medium italic text-[16px] text-[rgba(0,0,0,0.6)]">Head Coach & Founder</p>
+                        <p className="font-medium text-[16px] text-[rgba(0,0,0,0.6)]">Built this legacy on the belief that consistent effort yields uncompromising strength.</p>
+                    </div>
+                </div>
+            </div>
+        </section>
     )
 }
