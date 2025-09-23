85a60a3d12934fafcbf7cb8b1cfd7b6c59aceeac
AntoOfo <148264049+AntoOfo@users.noreply.github.com>
2025-09-23 20:01:48 +0100

    membership data populated

---
+++ data/membershipData.js
@@ -0,0 +1,19 @@
+export const membershipData = [
+    {
+        type: "Month-to-Month",
+        price: 89,
+        desc: "Full facility access, unlimited classes. Cancel anytime."
+    },
+
+    {
+        type: "6-Month Contract",
+        price: 69,
+        desc: "Includes free trainer onboarding session."
+    },
+
+    {
+        type: "12-Month Contract",
+        price: 49,
+        desc: "Includes Sauna/Recovery Lounge access. Best Value."
+    }
+]
