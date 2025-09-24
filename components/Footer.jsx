a16db6a3ec3d5bef07b3c71f3db818c04103b5a0
AntoOfo <148264049+AntoOfo@users.noreply.github.com>
2025-09-24 22:25:03 +0100

    cursor hover added to footer elements

--- components/Footer.jsx
+++ components/Footer.jsx
@@ -5,27 +5,27 @@ export default function Footer() {
                 <div className="flex justify-between">
                     <div>
                         <h2 className="font-semibold text-2xl text-white">Explore</h2>
-                        <p className="mt-6 font-normal text-[20px] text-[rgba(255,255,255,0.8)]">Our Mission</p>
-                        <p className="font-normal text-[20px] text-[rgba(255,255,255,0.8)]">Facility Tour</p>
-                        <p className="font-normal text-[20px] text-[rgba(255,255,255,0.8)]">Membership Plans</p>
-                        <p className="font-normal text-[20px] text-[rgba(255,255,255,0.8)]">Meet The Team</p>
-                        <p className="font-normal text-[20px] text-[rgba(255,255,255,0.8)]">Testimonials</p>
+                        <p className="mt-6 font-normal text-[20px] text-[rgba(255,255,255,0.8)] hover:cursor-pointer">Our Mission</p>
+                        <p className="font-normal text-[20px] text-[rgba(255,255,255,0.8)] hover:cursor-pointer">Facility Tour</p>
+                        <p className="font-normal text-[20px] text-[rgba(255,255,255,0.8)] hover:cursor-pointer">Membership Plans</p>
+                        <p className="font-normal text-[20px] text-[rgba(255,255,255,0.8)] hover:cursor-pointer">Meet The Team</p>
+                        <p className="font-normal text-[20px] text-[rgba(255,255,255,0.8)] hover:cursor-pointer">Testimonials</p>
                     </div>
 
                     <div>
                         <h2 className="font-semibold text-2xl text-white">Our Policies</h2>
-                        <p className="mt-6 font-normal text-[20px] text-[rgba(255,255,255,0.8)]">Privacy Policy</p>
-                        <p className="font-normal text-[20px] text-[rgba(255,255,255,0.8)]">Health Commitment Statement</p>
-                        <p className="font-normal text-[20px] text-[rgba(255,255,255,0.8)]">Terms & Conditions</p>
-                        <p className="font-normal text-[20px] text-[rgba(255,255,255,0.8)]">Modern Slavery Statement</p>
-                        <p className="font-normal text-[20px] text-[rgba(255,255,255,0.8)]">Environmental Policy</p>
+                        <p className="mt-6 font-normal text-[20px] text-[rgba(255,255,255,0.8)] hover:cursor-pointer">Privacy Policy</p>
+                        <p className="font-normal text-[20px] text-[rgba(255,255,255,0.8)] hover:cursor-pointer">Health Commitment Statement</p>
+                        <p className="font-normal text-[20px] text-[rgba(255,255,255,0.8)] hover:cursor-pointer">Terms & Conditions</p>
+                        <p className="font-normal text-[20px] text-[rgba(255,255,255,0.8)] hover:cursor-pointer">Modern Slavery Statement</p>
+                        <p className="font-normal text-[20px] text-[rgba(255,255,255,0.8)] hover:cursor-pointer">Environmental Policy</p>
                     </div>
 
                     <div>
                         <h2 className="font-semibold text-2xl text-white">Connect</h2>
-                        <p className="mt-6 font-normal text-[20px] text-[rgba(255,255,255,0.8)]">Location & Hours</p>
-                        <p className="font-normal text-[20px] text-[rgba(255,255,255,0.8)]">Contact Us</p>
-                        <p className="font-normal text-[20px] text-[rgba(255,255,255,0.8)]">FAQ</p>
+                        <p className="mt-6 font-normal text-[20px] text-[rgba(255,255,255,0.8)] hover:cursor-pointer">Location & Hours</p>
+                        <p className="font-normal text-[20px] text-[rgba(255,255,255,0.8)] hover:cursor-pointer">Contact Us</p>
+                        <p className="font-normal text-[20px] text-[rgba(255,255,255,0.8)] hover:cursor-pointer">FAQ</p>
                     </div>
                 </div>
             </div>
