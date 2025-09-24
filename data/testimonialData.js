10b6ccf4bfd74c36d4f4293a3b5fba75375a0f01
AntoOfo <148264049+AntoOfo@users.noreply.github.com>
2025-09-24 21:21:47 +0100

    testimonial data file created and populated

---
+++ data/testimonialData.js
@@ -0,0 +1,26 @@
+import Sarah from "../images/reviewer_1.jpg"
+import Mark from "../images/reviewer_2.jpg"
+import Jess from "../images/reviewer_3.jpg"
+
+export const testimonialData = [
+    {
+        image: Sarah,
+        review: '"Ive tried a few gyms, but this is the first one where I actually feel like showing up. The equipment is top-notch, and the lack of judgment made all the difference. I hit my first major strength goal here!"',
+        name: "Sarah K",
+        role: "6-Month Contract"
+    },
+
+    {
+        image: Mark,
+        review: '"The coaching staff is next-level. I signed up for the 12-month plan for the value, but the personalized attention from the trainers is why I am staying. Worth every penny."',
+        name: "Mark D",
+        role: "12-Month Contract"
+    },
+
+    {
+        image: Jess,
+        review: '"Jasmine’s classes are challenging but seriously fun, and the energy in the studio is incredible. It truly feels like a genuine community, which keeps me motivated every week."',
+        name: "Jess V",
+        role: "Group Class Member"
+    }
+]
