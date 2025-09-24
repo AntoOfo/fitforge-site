16207ceb274e8146174bbc1eb9685d63203bfccf
AntoOfo <148264049+AntoOfo@users.noreply.github.com>
2025-09-24 22:08:31 +0100

    structuring footer

--- components/Footer.jsx
+++ components/Footer.jsx
@@ -1,5 +1,13 @@
 export default function Footer() {
     return (
-        <h1>im the footer lol</h1>
+        <section className="w-full py-20 bg-[rgba(44,44,44,1)] font-[Poppins]">
+            <div className="ml-32 mr-32">
+                <div className="flex">
+                    <div>
+                        
+                    </div>
+                </div>
+            </div>
+        </section>
     )
 }
