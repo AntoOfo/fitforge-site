c8247c14d7c4788d2e098113d29714ecd1f69cde
AntoOfo <148264049+AntoOfo@users.noreply.github.com>
2025-09-24 22:20:14 +0100

    footer section structure sorted

--- components/Footer.jsx
+++ components/Footer.jsx
@@ -2,9 +2,30 @@ export default function Footer() {
     return (
         <section className="w-full py-20 bg-[rgba(44,44,44,1)] font-[Poppins]">
             <div className="ml-32 mr-32">
-                <div className="flex">
+                <div className="flex justify-between">
                     <div>
-                        
+                        <h2 className="font-semibold text-2xl text-white">Explore</h2>
+                        <p className="mt-6 font-normal text-[20px] text-[rgba(255,255,255,0.8)]">Our Mission</p>
+                        <p className="font-normal text-[20px] text-[rgba(255,255,255,0.8)]">Facility Tour</p>
+                        <p className="font-normal text-[20px] text-[rgba(255,255,255,0.8)]">Membership Plans</p>
+                        <p className="font-normal text-[20px] text-[rgba(255,255,255,0.8)]">Meet The Team</p>
+                        <p className="font-normal text-[20px] text-[rgba(255,255,255,0.8)]">Testimonials</p>
+                    </div>
+
+                    <div>
+                        <h2 className="font-semibold text-2xl text-white">Our Policies</h2>
+                        <p className="mt-6 font-normal text-[20px] text-[rgba(255,255,255,0.8)]">Privacy Policy</p>
+                        <p className="font-normal text-[20px] text-[rgba(255,255,255,0.8)]">Health Commitment Statement</p>
+                        <p className="font-normal text-[20px] text-[rgba(255,255,255,0.8)]">Terms & Conditions</p>
+                        <p className="font-normal text-[20px] text-[rgba(255,255,255,0.8)]">Modern Slavery Statement</p>
+                        <p className="font-normal text-[20px] text-[rgba(255,255,255,0.8)]">Environmental Policy</p>
+                    </div>
+
+                    <div>
+                        <h2 className="font-semibold text-2xl text-white">Connect</h2>
+                        <p className="mt-6 font-normal text-[20px] text-[rgba(255,255,255,0.8)]">Location & Hours</p>
+                        <p className="font-normal text-[20px] text-[rgba(255,255,255,0.8)]">Contact Us</p>
+                        <p className="font-normal text-[20px] text-[rgba(255,255,255,0.8)]">FAQ</p>
                     </div>
                 </div>
             </div>
