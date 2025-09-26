78f90584c464fcefc53a2d66bbc5b2f9800f4710
AntoOfo <148264049+AntoOfo@users.noreply.github.com>
2025-09-26 18:53:55 +0100

    aos anim set for join us section

--- components/Join.jsx
+++ components/Join.jsx
@@ -4,12 +4,15 @@ export default function Join() {
   return (
     <section className="mt-16 w-full flex flex-col items-center font-[Poppins]">
       <div className="ml-32 mr-32">
-        <h2 className="text-center font-bold text-[40px] text-[rgba(192,57,43,1)]">
+        <h2
+          data-aos="fade-up"
+          className="text-center font-bold text-[40px] text-[rgba(192,57,43,1)]"
+        >
           Why Join Us?
         </h2>
         <div className="mt-12 mb-16 flex justify-between gap-[221px] w-full">
           {joinData.map((join) => (
-            <div className="flex flex-col items-center">
+            <div data-aos="fade-up" className="flex flex-col items-center">
               <img src={join.image} className="w-[75px]" />
               <h3 className="mt-2 font-medium text-2xl text-[rgba(192,57,43,1)]">
                 {join.text}
