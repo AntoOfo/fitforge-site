6eb5bcaae39f4c034220844e31d5323340fc425e
AntoOfo <148264049+AntoOfo@users.noreply.github.com>
2025-09-27 19:07:36 +0100

    swiper added, shadcn nav elements/logic and imports removed. need to sort nav buttons for swiper

--- components/Explore.jsx
+++ components/Explore.jsx
@@ -2,27 +2,9 @@ import React from "react";
 import { exploreData } from "../data/exploreData";
 import { Card, CardContent } from "@/components/ui/card";
 import { Swiper, SwiperSlide } from "swiper/react";
-import { Navigation, Pagination, Lazy } from "swiper";
 import "swiper/css";
 
 export default function Explore() {
-  const [api, setApi] = React.useState(null);
-  const [current, setCurrent] = React.useState(0);
-  const [count, setCount] = React.useState(0);
-
-  React.useEffect(() => {
-    if (!api) {
-      return;
-    }
-
-    setCount(api.scrollSnapList().length);
-    setCurrent(api.selectedScrollSnap() + 1);
-
-    api.on("select", () => {
-      setCurrent(api.selectedScrollSnap() + 1);
-    });
-  }, [api]);
-
   return (
     <section id="services" className="w-full font-[Poppins]">
       <div className="flex flex-col py-16 items-center">
@@ -33,24 +15,26 @@ export default function Explore() {
           Explore The Gym
         </h2>
 
-        <Swiper spaceBetween={24} slidesPerView={2}>
-          {exploreData.map((explore) => (
-            <Card className="border-0 shadow-none">
-              <CardContent className="flex flex-col p-4">
-                <img
-                  src={explore.image}
-                  alt={explore.title}
-                  loading="lazy"
-                  className="w-full h-[348px] object-cover rounded-lg border-3 border-[rgba(192,57,43,1)]"
-                />
-                <h3 className="mt-4 font-bold text-[32px] text-[rgba(192,57,43,0.8)]">
-                  {explore.title}
-                </h3>
-                <p className="mt-2 w-full font-normal text-[20px] text-[rgba(0,0,0,0.8)]">
-                  {explore.desc}
-                </p>
-              </CardContent>
-            </Card>
+        <Swiper spaceBetween={24} slidesPerView={2} className="max-w-[1300px]">
+          {exploreData.map((explore, index) => (
+            <SwiperSlide key={index}>
+              <Card className="border-0 shadow-none">
+                <CardContent className="flex flex-col p-4">
+                  <img
+                    src={explore.image}
+                    alt={explore.title}
+                    loading="lazy"
+                    className="w-full h-[348px] object-cover rounded-lg border-3 border-[rgba(192,57,43,1)]"
+                  />
+                  <h3 className="mt-4 font-bold text-[32px] text-[rgba(192,57,43,0.8)]">
+                    {explore.title}
+                  </h3>
+                  <p className="mt-2 w-full font-normal text-[20px] text-[rgba(0,0,0,0.8)]">
+                    {explore.desc}
+                  </p>
+                </CardContent>
+              </Card>
+            </SwiperSlide>
           ))}
         </Swiper>
       </div>
