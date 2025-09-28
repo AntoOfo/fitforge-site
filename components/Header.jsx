b477dc88b53c9be0e396e1fe6c9501df361aba4a
AntoOfo <148264049+AntoOfo@users.noreply.github.com>
2025-09-28 22:46:23 +0100

    text, image and header size clamp set up

--- components/Header.jsx
+++ components/Header.jsx
@@ -5,9 +5,14 @@ export default function Header() {
   const navLinks = ["HOME", "ABOUT", "SERVICES", "PRICING", "CONTACT"];
 
   return (
-    <header className="sticky top-0 z-50 flex items-center justify-between px-40 h-20 w-full bg-white">
-      <img src={Logo} alt="FitForge logo" loading="lazy" className="w-20" />
-      <nav className="flex ml-[483px] gap-9 font-[Poppins] font-normal text-lg">
+    <header className="sticky top-0 z-50 flex items-center justify-between px-[clamp(5rem,105vw,10rem)] h-[clamp(2rem,5.2vw,5rem)] w-full bg-white">
+      <img
+        src={Logo}
+        alt="FitForge logo"
+        loading="lazy"
+        className="w-[clamp(2.5rem,5.2vw,5rem)]"
+      />
+      <nav className="flex ml-[483px] gap-9 font-[Poppins] font-normal text-[clamp(0.55rem,1.2vw,1.125rem)]">
         {navLinks.map((link) => (
           <Link
             key={link}
