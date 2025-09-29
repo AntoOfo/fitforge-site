4dadf4a356dfa80818efd653ffd70dbd1a039720
AntoOfo <148264049+AntoOfo@users.noreply.github.com>
2025-09-29 19:19:29 +0100

    join section fully responsive. flex row turned to grid with 4 cols and based resposiveness from that. clamps set up

--- components/Join.jsx
+++ components/Join.jsx
@@ -6,15 +6,19 @@ export default function Join() {
       <div className="ml-32 mr-32">
         <h2
           data-aos="fade-up"
-          className="text-center font-bold text-[40px] text-[rgba(192,57,43,1)]"
+          className="text-center font-bold text-[clamp(1rem,2.6vw,2.5rem)] text-[rgba(192,57,43,1)] [@media(max-width:400px)]:mb-14"
         >
           Why Join Us?
         </h2>
-        <div className="mt-12 mb-16 flex justify-between gap-[221px] w-full">
+        <div className="mt-12 mb-16 grid grid-cols-4 [@media(max-width:624px)]:grid-cols-2 [@media(max-width:624px)]:gap-18 [@media(max-width:624px)]:mt-6 [@media(max-width:400px)]:flex [@media(max-width:400px)]:flex-col  justify-between gap-[clamp(2rem,14.5vw,13.5rem)] w-full">
           {joinData.map((join) => (
             <div data-aos="fade-up" className="flex flex-col items-center">
-              <img src={join.image} loading="lazy" className="w-[75px]" />
-              <h3 className="mt-2 font-medium text-2xl text-[rgba(192,57,43,1)]">
+              <img
+                src={join.image}
+                loading="lazy"
+                className="w-[clamp(2.5rem,6vw,4.6rem)]"
+              />
+              <h3 className="mt-2 font-medium text-[clamp(0.5rem,1.5vw,1.5rem)] text-[rgba(192,57,43,1)] whitespace-nowrap w-auto">
                 {join.text}
               </h3>
             </div>
