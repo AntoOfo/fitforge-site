afa9b2a266df852410fe01f8433afe3ae01e757c
AntoOfo <148264049+AntoOfo@users.noreply.github.com>
2025-10-01 21:04:40 +0100

    all text in footer clamped

--- components/Footer.jsx
+++ components/Footer.jsx
@@ -2,57 +2,63 @@ export default function Footer() {
   return (
     <section
       id="contact"
-      className="w-full py-20 bg-[rgba(44,44,44,1)] font-[Poppins]"
+      className="w-full py-[clamp(2.5rem,5.2vw,5rem)] bg-[rgba(44,44,44,1)] font-[Poppins]"
     >
-      <div className="ml-32 mr-32">
+      <div className="ml-[clamp(4rem,8.4vw,8rem)] mr-[clamp(4rem,8.4vw,8rem)]">
         <div className="flex justify-between">
           <div>
-            <h2 className="font-semibold text-2xl text-white">Explore</h2>
-            <p className="mt-6 font-normal text-[20px] text-[rgba(255,255,255,0.8)] hover:cursor-pointer">
+            <h2 className="font-semibold text-[clamp(0.75rem,1.5vw,1.5re[clamp(0.75rem,1.5vw,1.5rem)] text-white">
+              Explore
+            </h2>
+            <p className="mt-6 font-normal text-[clamp(0.6rem,1.3vw,1.25rem)] text-[rgba(255,255,255,0.8)] hover:cursor-pointer">
               Our Mission
             </p>
-            <p className="font-light text-[20px] text-[rgba(255,255,255,0.8)] hover:cursor-pointer">
+            <p className="font-light text-[clamp(0.6rem,1.3vw,1.25rem)] text-[rgba(255,255,255,0.8)] hover:cursor-pointer">
               Facility Tour
             </p>
-            <p className="font-light text-[20px] text-[rgba(255,255,255,0.8)] hover:cursor-pointer">
+            <p className="font-light text-[clamp(0.6rem,1.3vw,1.25rem)] text-[rgba(255,255,255,0.8)] hover:cursor-pointer">
               Membership Plans
             </p>
-            <p className="font-light text-[20px] text-[rgba(255,255,255,0.8)] hover:cursor-pointer">
+            <p className="font-light text-[clamp(0.6rem,1.3vw,1.25rem)] text-[rgba(255,255,255,0.8)] hover:cursor-pointer">
               Meet The Team
             </p>
-            <p className="font-light text-[20px] text-[rgba(255,255,255,0.8)] hover:cursor-pointer">
+            <p className="font-light text-[clamp(0.6rem,1.3vw,1.25rem)] text-[rgba(255,255,255,0.8)] hover:cursor-pointer">
               Testimonials
             </p>
           </div>
 
           <div>
-            <h2 className="font-semibold text-2xl text-white">Our Policies</h2>
-            <p className="mt-6 font-normal text-[20px] text-[rgba(255,255,255,0.8)] hover:cursor-pointer">
+            <h2 className="font-semibold text-[clamp(0.75rem,1.5vw,1.5rem)] text-white">
+              Our Policies
+            </h2>
+            <p className="mt-6 font-normal text-[clamp(0.6rem,1.3vw,1.25rem)] text-[rgba(255,255,255,0.8)] hover:cursor-pointer">
               Privacy Policy
             </p>
-            <p className="font-light text-[20px] text-[rgba(255,255,255,0.8)] hover:cursor-pointer">
+            <p className="font-light text-[clamp(0.6rem,1.3vw,1.25rem)] text-[rgba(255,255,255,0.8)] hover:cursor-pointer">
               Health Commitment Statement
             </p>
-            <p className="font-light text-[20px] text-[rgba(255,255,255,0.8)] hover:cursor-pointer">
+            <p className="font-light text-[clamp(0.6rem,1.3vw,1.25rem)] text-[rgba(255,255,255,0.8)] hover:cursor-pointer">
               Terms & Conditions
             </p>
-            <p className="font-light text-[20px] text-[rgba(255,255,255,0.8)] hover:cursor-pointer">
+            <p className="font-light text-[clamp(0.6rem,1.3vw,1.25rem)] text-[rgba(255,255,255,0.8)] hover:cursor-pointer">
               Modern Slavery Statement
             </p>
-            <p className="font-light text-[20px] text-[rgba(255,255,255,0.8)] hover:cursor-pointer">
+            <p className="font-light text-[clamp(0.6rem,1.3vw,1.25rem)] text-[rgba(255,255,255,0.8)] hover:cursor-pointer">
               Environmental Policy
             </p>
           </div>
 
           <div>
-            <h2 className="font-semibold text-2xl text-white">Connect</h2>
-            <p className="mt-6 font-light text-[20px] text-[rgba(255,255,255,0.8)] hover:cursor-pointer">
+            <h2 className="font-semibold text-[clamp(0.75rem,1.5vw,1.5rem)] text-white">
+              Connect
+            </h2>
+            <p className="mt-6 font-light text-[clamp(0.6rem,1.3vw,1.25rem)] text-[rgba(255,255,255,0.8)] hover:cursor-pointer">
               Location & Hours
             </p>
-            <p className="font-light text-[20px] text-[rgba(255,255,255,0.8)] hover:cursor-pointer">
+            <p className="font-light text-[clamp(0.6rem,1.3vw,1.25rem)] text-[rgba(255,255,255,0.8)] hover:cursor-pointer">
               Contact Us
             </p>
-            <p className="font-light text-[20px] text-[rgba(255,255,255,0.8)] hover:cursor-pointer">
+            <p className="font-light text-[clamp(0.6rem,1.3vw,1.25rem)] text-[rgba(255,255,255,0.8)] hover:cursor-pointer">
               FAQ
             </p>
           </div>
