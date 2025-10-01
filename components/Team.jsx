204ba114181295f09aee4ee2995600ddbe94ce9b
AntoOfo <148264049+AntoOfo@users.noreply.github.com>
2025-10-01 17:44:58 +0100

    grid, gap, padding, text and bg colour of team member pills altered

--- components/Team.jsx
+++ components/Team.jsx
@@ -10,11 +10,11 @@ export default function Team() {
           <h1 className="font-bold text-[clamp(1rem,2.6vw,2.5rem)] text-[rgba(192,57,43,1)]">
             Meet The Team
           </h1>
-          <div className="grid grid-cols-4 gap-24 mt-[clamp(2.25rem,4.7vw,4.5rem)]">
+          <div className="grid grid-cols-4 [@media(max-width:760px)]:grid-cols-2 gap-[clamp(3.5rem,7.3vw,7rem)] mt-[clamp(2.25rem,4.7vw,4.5rem)]">
             {teamData.map((team) => (
               <div
                 key={team.name}
-                className="justify-items-center py-[clamp(1.5rem,3.1vw,3rem)] px-[clamp(0.5rem,1vw,1rem)] rounded-[134px] bg-[rgba(239,239,239,1)] shadow-[0_2px_8px_rgba(0,0,0,0.12)]"
+                className="justify-items-center py-[clamp(1.5rem,3.1vw,3rem)] px-[clamp(0.5rem,1vw,1rem)] [@media(max-width:760px)]:px-8 rounded-[clamp(4.1rem,8.8vw,8.375rem)] bg-[#efefef6b] shadow-[0_2px_8px_rgba(0,0,0,0.12)]"
               >
                 <img
                   src={team.image}
@@ -28,7 +28,7 @@ export default function Team() {
                 <p className="font-medium italic text-[clamp(0.5rem,1vw,1rem)] text-[rgba(0,0,0,0.6)]">
                   {team.role}
                 </p>
-                <p className="mt-[clamp(0.8rem,1.7vw,1.625rem)] font-medium text-center text-[clamp(0.5rem,1vw,1rem)] w-[clamp(5.8rem,12.2vw,11.68rem)] text-[rgba(0,0,0,0.6)]">
+                <p className="mt-[clamp(0.8rem,1.7vw,1.625rem)] font-medium text-center text-[clamp(0.5rem,1vw,1rem)] w-[clamp(5.8rem,12.2vw,11.68rem)] [@media(max-width:760px)]:w-full text-[rgba(0,0,0,0.6)]">
                   {team.about}
                 </p>
               </div>
